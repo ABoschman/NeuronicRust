6cbfd96e0093c0fc4cf23cd224f14ce7b6ca3717
Arjan Boschman <arjan@boschman.io>
2017-11-17 16:45:42 +0100

    Initialised a new Cargo project.

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,6 +1,17 @@
+#[allow(dead_code)]
+
+fn hello_world() -> bool {
+    true
+}
+
+
 #[cfg(test)]
 mod tests {
+
+    use hello_world;
+
     #[test]
     fn it_works() {
+        assert!(hello_world());
     }
 }
